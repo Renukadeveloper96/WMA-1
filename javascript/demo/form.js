2a47b6e2442831aa8e7dd322d73b4ede6d3d2024
Renukadeveloper96 <renukamaradagi@gmail.com>
2023-03-07 17:37:23 +0530

    first

---
+++ javascript/demo/form.js
@@ -0,0 +1,26 @@
+function fillId(){
+    var id=Math.floor(Math.random()*100000)
+    document.getElementById('orderid').value=id;
+}
+function nameToUpper(){
+    var fname=document.getElementById('firstname').value;
+    fname=fname.charAt(0).toUpperCase()+fname.slice(1).toLowerCase();
+    document.getElementById('firstname').value=fname;
+}
+
+function checkPassword(){
+    var pwd=document.getElementById('password').value;
+    if(pwd.length<15){
+        if(pwd.length<8){
+            document.getElementById('pout').innerText="Min length of password is 8";
+            document.getElementById('ppout').style.display="block";
+            document.getElementById('ppout').style.backgroundColor="red";
+        }else{
+            document.getElementById('pout').innerText="";
+            document.getElementById('ppout').style.backgroundColor="orange";
+
+        }  
+    }else{
+        document.getElementById("ppout").style.backgroundColor="green";
+    }
+}
